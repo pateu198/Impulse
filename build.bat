43c9d7c7987f2a11ab83be7fdd063b203c811c9e
pateu198 <92146283+pateu198@users.noreply.github.com>
2023-04-30 11:58:05 +0300

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,246 @@
+@echo off
+mode con: cols=100 lines=30
+title Impulse
+color 09
+cls
+timeout /t 2 >nul
+title Impulse Loading 0-----------
+color 0C
+cls
+
+cls                                                                                                                                               
+echo IIIIIIIIII                                                             
+echo I::::::::I                                                             
+echo I::::::::I                                                             
+echo II::::::II                                                             
+echo   I::::I     mmmmmmm    mmmmmmm   ppppp   ppppppppp       ssssssssss   
+echo   I::::I   mm:::::::m  m:::::::mm p::::ppp:::::::::p    ss::::::::::s  
+echo   I::::I  m::::::::::mm::::::::::mp:::::::::::::::::p ss:::::::::::::s 
+echo   I::::I  m:::::mmm::::::mmm:::::m p:::::p     p:::::p s:::::s  ssssss 
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p   s::::::s      
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p      s::::::s   
+echo   I::::I  m::::m   m::::m   m::::m p:::::p    p::::::pssssss   s:::::s 
+echo II::::::IIm::::m   m::::m   m::::m p:::::ppppp:::::::ps:::::ssss::::::s
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::::p s::::::::::::::s 
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::pp   s:::::::::::ss  
+echo IIIIIIIIIImmmmmm   mmmmmm   mmmmmm p::::::pppppppp      sssssssssss    
+echo   Made with love by patew and darkk p:::::p                             
+echo                                     p:::::p                             
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    ppppppppp                            
+ 
+timeout /t 5 >nul
+title Impulse Loading 00000-------
+color 0D
+
+cls                                                                                                                                               
+echo IIIIIIIIII                                                             
+echo I::::::::I                                                             
+echo I::::::::I                                                             
+echo II::::::II                                                             
+echo   I::::I     mmmmmmm    mmmmmmm   ppppp   ppppppppp       ssssssssss   
+echo   I::::I   mm:::::::m  m:::::::mm p::::ppp:::::::::p    ss::::::::::s  
+echo   I::::I  m::::::::::mm::::::::::mp:::::::::::::::::p ss:::::::::::::s 
+echo   I::::I  m:::::mmm::::::mmm:::::m p:::::p     p:::::p s:::::s  ssssss 
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p   s::::::s      
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p      s::::::s   
+echo   I::::I  m::::m   m::::m   m::::m p:::::p    p::::::pssssss   s:::::s 
+echo II::::::IIm::::m   m::::m   m::::m p:::::ppppp:::::::ps:::::ssss::::::s
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::::p s::::::::::::::s 
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::pp   s:::::::::::ss  
+echo IIIIIIIIIImmmmmm   mmmmmm   mmmmmm p::::::pppppppp      sssssssssss    
+echo   Made with love by patew and darkk p:::::p                             
+echo                                     p:::::p                             
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    ppppppppp                            
+
+timeout /t 5 >nul
+
+title Impulse Loading 0000000-----
+color 0C
+cls                                                                                                                                               
+echo IIIIIIIIII                                                             
+echo I::::::::I                                                             
+echo I::::::::I                                                             
+echo II::::::II                                                             
+echo   I::::I     mmmmmmm    mmmmmmm   ppppp   ppppppppp       ssssssssss   
+echo   I::::I   mm:::::::m  m:::::::mm p::::ppp:::::::::p    ss::::::::::s  
+echo   I::::I  m::::::::::mm::::::::::mp:::::::::::::::::p ss:::::::::::::s 
+echo   I::::I  m:::::mmm::::::mmm:::::m p:::::p     p:::::p s:::::s  ssssss 
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p   s::::::s      
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p      s::::::s   
+echo   I::::I  m::::m   m::::m   m::::m p:::::p    p::::::pssssss   s:::::s 
+echo II::::::IIm::::m   m::::m   m::::m p:::::ppppp:::::::ps:::::ssss::::::s
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::::p s::::::::::::::s 
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::pp   s:::::::::::ss  
+echo IIIIIIIIIImmmmmm   mmmmmm   mmmmmm p::::::pppppppp      sssssssssss    
+echo   Made with love by patew and darkk p:::::p                             
+echo                                     p:::::p                             
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    ppppppppp                            
+
+timeout /t 1 >nul
+title Impulse Loading 0000000-----
+color 0B
+cls                                                                                                                                               
+echo IIIIIIIIII                                                             
+echo I::::::::I                                                             
+echo I::::::::I                                                             
+echo II::::::II                                                             
+echo   I::::I     mmmmmmm    mmmmmmm   ppppp   ppppppppp       ssssssssss   
+echo   I::::I   mm:::::::m  m:::::::mm p::::ppp:::::::::p    ss::::::::::s  
+echo   I::::I  m::::::::::mm::::::::::mp:::::::::::::::::p ss:::::::::::::s 
+echo   I::::I  m:::::mmm::::::mmm:::::m p:::::p     p:::::p s:::::s  ssssss 
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p   s::::::s      
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p      s::::::s   
+echo   I::::I  m::::m   m::::m   m::::m p:::::p    p::::::pssssss   s:::::s 
+echo II::::::IIm::::m   m::::m   m::::m p:::::ppppp:::::::ps:::::ssss::::::s
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::::p s::::::::::::::s 
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::pp   s:::::::::::ss  
+echo IIIIIIIIIImmmmmm   mmmmmm   mmmmmm p::::::pppppppp      sssssssssss    
+echo   Made with love by patew and darkk p:::::p                             
+echo                                     p:::::p                             
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    ppppppppp                            
+timeout /t 1 >nul
+title Impulse Loading 000000000---
+color 0C
+cls                                                                                                                                               
+echo IIIIIIIIII                                                             
+echo I::::::::I                                                             
+echo I::::::::I                                                             
+echo II::::::II                                                             
+echo   I::::I     mmmmmmm    mmmmmmm   ppppp   ppppppppp       ssssssssss   
+echo   I::::I   mm:::::::m  m:::::::mm p::::ppp:::::::::p    ss::::::::::s  
+echo   I::::I  m::::::::::mm::::::::::mp:::::::::::::::::p ss:::::::::::::s 
+echo   I::::I  m:::::mmm::::::mmm:::::m p:::::p     p:::::p s:::::s  ssssss 
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p   s::::::s      
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p      s::::::s   
+echo   I::::I  m::::m   m::::m   m::::m p:::::p    p::::::pssssss   s:::::s 
+echo II::::::IIm::::m   m::::m   m::::m p:::::ppppp:::::::ps:::::ssss::::::s
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::::p s::::::::::::::s 
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::pp   s:::::::::::ss  
+echo IIIIIIIIIImmmmmm   mmmmmm   mmmmmm p::::::pppppppp      sssssssssss    
+echo   Made with love by patew and darkk p:::::p                             
+echo                                     p:::::p                             
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    ppppppppp                            
+timeout /t 1 >nul
+title Impulse Loading 0000000000-
+color 0E
+cls                                                                                                                                               
+echo IIIIIIIIII                                                             
+echo I::::::::I                                                             
+echo I::::::::I                                                             
+echo II::::::II                                                             
+echo   I::::I     mmmmmmm    mmmmmmm   ppppp   ppppppppp       ssssssssss   
+echo   I::::I   mm:::::::m  m:::::::mm p::::ppp:::::::::p    ss::::::::::s  
+echo   I::::I  m::::::::::mm::::::::::mp:::::::::::::::::p ss:::::::::::::s 
+echo   I::::I  m:::::mmm::::::mmm:::::m p:::::p     p:::::p s:::::s  ssssss 
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p   s::::::s      
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p      s::::::s   
+echo   I::::I  m::::m   m::::m   m::::m p:::::p    p::::::pssssss   s:::::s 
+echo II::::::IIm::::m   m::::m   m::::m p:::::ppppp:::::::ps:::::ssss::::::s
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::::p s::::::::::::::s 
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::pp   s:::::::::::ss  
+echo IIIIIIIIIImmmmmm   mmmmmm   mmmmmm p::::::pppppppp      sssssssssss    
+echo   Made with love by patew and darkk p:::::p                             
+echo                                     p:::::p                             
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    ppppppppp                            
+timeout /t 1 >nul
+title Impulse Loading 0000000000-
+color 0D
+cls                                                                                                                                               
+echo IIIIIIIIII                                                             
+echo I::::::::I                                                             
+echo I::::::::I                                                             
+echo II::::::II                                                             
+echo   I::::I     mmmmmmm    mmmmmmm   ppppp   ppppppppp       ssssssssss   
+echo   I::::I   mm:::::::m  m:::::::mm p::::ppp:::::::::p    ss::::::::::s  
+echo   I::::I  m::::::::::mm::::::::::mp:::::::::::::::::p ss:::::::::::::s 
+echo   I::::I  m:::::mmm::::::mmm:::::m p:::::p     p:::::p s:::::s  ssssss 
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p   s::::::s      
+echo   I::::I  m::::m   m::::m   m::::m p:::::p     p:::::p      s::::::s   
+echo   I::::I  m::::m   m::::m   m::::m p:::::p    p::::::pssssss   s:::::s 
+echo II::::::IIm::::m   m::::m   m::::m p:::::ppppp:::::::ps:::::ssss::::::s
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::::p s::::::::::::::s 
+echo I::::::::Im::::m   m::::m   m::::m p::::::::::::::pp   s:::::::::::ss  
+echo IIIIIIIIIImmmmmm   mmmmmm   mmmmmm p::::::pppppppp      sssssssssss    
+echo   Made with love by patew and darkk p:::::p                             
+echo                                     p:::::p                             
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    p:::::::p                            
+echo                                    ppppppppp                            
+ timeout /t 1 >nul                                                      
+
+title Impulse Loading 0000000000-
+color 0E
+cls
+echo   Made with love by patew and darkk 
+
+echo   Made with love by patew and darkk 
+
+echo   Made with love by patew and darkk 
+
+echo   Made with love by patew and darkk 
+
+echo   Made with love by patew and darkk 
+ 
+echo   Made with love by patew and darkk 
+timeout /t 5 >nul
+title Impulse Loading 00000000000
+color 0B
+cls
+
+echo   tiktok : notfoundedre
+
+echo   tiktok : notfoundedre
+
+echo   tiktok : notfoundedre
+
+echo   discord : https://discord.gg/75QFPztfwk
+
+echo   discord : https://discord.gg/75QFPztfwk
+
+echo   discord : https://discord.gg/75QFPztfwk
+                                                     
+timeout /t 5 >nul													 
+python --version 2>&1 | findstr " 3.11" >nul
+if %errorlevel% == 0 (
+    echo python 3.11.x and up are not supported by impulse. Please downgrade to python 3.10.x.
+    pause
+    exit
+)
+
+::fixed by K.Dot cause dif
+git --version 2>&1>nul
+if %errorlevel% == 9009 (
+    echo git is either not installed or not added to path! You can install it here https://git-scm.com/download/win
+    pause
+    exit
+)
+
+py -3.10 -m pip uninstall -r interferences.txt
+py -3.10 -m pip install --upgrade -r requirements.txt
+
+cls
+
+if exist build rmdir /s /q build
+py -3.10 builder.py
+
+pause
+
